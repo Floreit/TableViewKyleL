1ca442d54db5586f48c54dc82bc1a15123132a17
test <kludford@e104-m-17.mchenry.edu>
2016-03-07 16:26:17 -0600

    first

--- tableViewKyleL/ViewController.swift
+++ tableViewKyleL/ViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    let table = [
+        ["Name" : "Fuller", "number": "1"],
+        ["Name" : "Nick", "number": "2"],
+        ["Name" : "Flo", "number": "3"],
+        ["Name" :"Justin", "number": "4"]
+    
+    
+    
+    
+    ]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return table.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cell, forIndexPath: indexPath)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
